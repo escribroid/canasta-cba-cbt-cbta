228750868aaa6c700d64d916fc9e40f0850cf3b2
escribroid <sebascraton@gmail.com>
2024-09-09 14:36:29 -0300

    init v0.2.17

--- js/app.js
+++ js/app.js
@@ -388,20 +388,30 @@ alquiler_in.addEventListener("keydown", function (e) {
 
 alquiler_in.addEventListener("input", function (e) {
     // // Permitir solo dígitos
+    const regex = /[0-9]/;
+    if (!regex.test(e.key)) {
+        e.preventDefault(); // Evitar que el carácter no permitido sea ingresado
+    }
+
+    //alquiler_in.value = alquiler_in.value.replace(/[^0-9]/g, "");
+});
+
+//alquiler_in.addEventListener("input", function (e) {
+    // // Permitir solo dígitos
     // const regex = /[0-9]/;
     // if (!regex.test(e.key)) {
     //     e.preventDefault(); // Evitar que el carácter no permitido sea ingresado
     // }
 
     // Permitir números (del 0 al 9) y la tecla Backspace
-    if ((e.key >= "0" && e.key <= "9") || e.key === "Backspace") {
-        // Permitir la entrada
-        return true;
-    } else {
-        // Evitar que se ingrese cualquier otra tecla
-        e.preventDefault();
-    }
-});
+    // if ((e.key >= "0" && e.key <= "9") || e.key === "Backspace") {
+    //     // Permitir la entrada
+    //     return true;
+    // } else {
+    //     // Evitar que se ingrese cualquier otra tecla
+    //     e.preventDefault();
+    // }
+//});
 
 // event INPUT ++++++++++++++++++++++++++++++++++++++++++++++++++++
 document.getElementById("alquiler_in").addEventListener("input", () => {
