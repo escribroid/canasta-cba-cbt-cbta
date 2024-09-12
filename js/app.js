40b4d122687f227a0227b6e7e5a13f8e452c4a08
escribroid <sebascraton@gmail.com>
2024-09-12 17:37:29 -0300

    init v0.2.36

--- js/app.js
+++ js/app.js
@@ -119,7 +119,9 @@ function addPersonToTable(gender, age_mostrar_table, canasta_b_alimentaria_perso
             maximumFractionDigits: 0,
         }
     )}</td><td class="p-1 detalles_delete ">
-    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 7V18C6 19.1046 6.89543 20 8 20H16C17.1046 20 18 19.1046 18 18V7M6 7H5M6 7H8M18 7H19M18 7H16M10 11V16M14 11V16M8 7V5C8 3.89543 8.89543 3 10 3H14C15.1046 3 16 3.89543 16 5V7M8 7H16" stroke="#ff4f00" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+    <svg 
+    class="detalles_delete_body"
+    width="20px" height="20px" viewBox="0 0 24 24" fill="#000000" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 7V18C6 19.1046 6.89543 20 8 20H16C17.1046 20 18 19.1046 18 18V7M6 7H5M6 7H8M18 7H19M18 7H16M10 11V16M14 11V16M8 7V5C8 3.89543 8.89543 3 10 3H14C15.1046 3 16 3.89543 16 5V7M8 7H16" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
     </td><td>
     <label for="">
     <select name="days_select" id="days_select">
