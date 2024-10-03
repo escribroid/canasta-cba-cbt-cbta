c895b5dfa1bd2c07a68e6231f5a72796eda55e80
escribroid <sebascraton@gmail.com>
2024-10-03 00:09:12 -0300

    beta v0.8.3

--- js/fetch-data.js
+++ js/fetch-data.js
@@ -1,14 +1,11 @@
 let url_ipc = "https://apis.datos.gob.ar/series/api/series/?ids=148.3_INIVELNAL_DICI_M_26&limit=5000&format=json";
 
 let ipc_percent = 0;
-let ipc_percent_txt = "";
+let ipc_percent_txt;
 
 fetch(url_ipc)
     .then((response) => response.json())
     .then((data_ipc) => {
-        //console.log("data_ipc_N", data_ipc.data[data_ipc.data.length-1][1])
-        //console.log("data_ipc_O", data_ipc.data[data_ipc.data.length-2][0]);
-
         let date_full = data_ipc.data[data_ipc.data.length - 1][0];
         //console.log("date_full", date_full);
         date_full.split("-");
@@ -18,30 +15,8 @@ fetch(url_ipc)
         let ipc_new = data_ipc.data[data_ipc.data.length - 1][1];
         let ipc_old = data_ipc.data[data_ipc.data.length - 2][1];
         ipc_percent = Math.round(((ipc_new - ipc_old) / ipc_old) * 100 * 100) / 100;
-        //console.log("ipc_percent", ipc_percent);
 
         ipc_percent_txt = ipc_percent.toLocaleString("es-AR", { minimumFractionDigits: 2 });
         document.querySelector(".indices_short_ipc_percent").textContent = ipc_percent_txt + "%";
-    })
-    .catch((error) => console.log(error));
-
-let url_cba_cbt =
-    "https://apis.datos.gob.ar/series/api/series/?ids=150.1_CSTA_BARIA_0_D_26,150.1_CSTA_BATAL_0_D_20&collapse=month&start_date=2016-01-01&limit=1000&format=json";
-
-fetch(url_cba_cbt)
-    .then((response) => response.json())
-    .then((data_cba_cbt) => {
-        // console.log("data_cba.Length", data_cba_cbt.data.length - 1);
-        // console.log("data_cba.Count", data_cba_cbt.count);
-        // console.log("data_cba.data[98]", data_cba_cbt.data[98][1]);
-        // console.log("DATA-CBA", data_cba_cbt.data[data_cba_cbt.data.length - 1][1]);
-
-        let cba = Math.round(data_cba_cbt.data[data_cba_cbt.data.length - 1][1] * 3.09);
-        const cba_top_short = document.querySelector(".indices_short_cba");
-        cba_top_short.innerHTML = `$${cba}`;
-
-        let cbt = Math.round(data_cba_cbt.data[data_cba_cbt.data.length - 1][2] * 3.09);
-        const cbt_top_short = document.querySelector(".indices_short_cbt");
-        cbt_top_short.innerHTML = `$${cbt}`;
     })
     .catch((error) => console.log(error));
--- js/mod-date.js
+++ js/mod-date.js
@@ -1,59 +1,93 @@
 export const year = new Date().getFullYear();
-let array_years = [];
-let selectedYear;
 
-for (let i = 0; i <= year; i++) {
-    if (year - i >= 2016) {
-        array_years.push(`
+function datesMain() {
+    let selectedYear;
+    let array_years = [];
+    let canasta_year_select = document.getElementById("canasta_date_year");
+    let canasta_month_select = document.getElementById("canasta_date_month");
+    let selectedMonth = canasta_month_select.value;
+
+    for (let i = 0; i <= year; i++) {
+        if (year - i >= 2016) {
+            array_years.push(`
             <option class="canasta_date_option" value="${year - i}"> ${year - i} </option>`);
-    }
-}
-
-let str_years = array_years.join("");
-let canasta_year_select = document.getElementById("canasta_date_year");
-let canasta_month_select = document.getElementById("canasta_date_month");
-let selectedMonth;
-
-canasta_year_select.innerHTML =
-    '<option value="anio" class="canasta_date_month_anio" selected disabled>• Año •</option>' + str_years;
-
-const option01_month = document.querySelector(".canasta_month_option[value='01']").cloneNode(true);
-const option02_month = document.querySelector(".canasta_month_option[value='02']").cloneNode(true);
-const option03_month = document.querySelector(".canasta_month_option[value='03']").cloneNode(true);
-
-// Guardar los options en un array
-const removedOptions = [option01_month, option02_month, option03_month];
-
-canasta_year_select.addEventListener("change", function () {
-    selectedYear = canasta_year_select.value;
-    console.log("selectedYear:", selectedYear);
-    console.log("Mes seleccionado:", selectedMonth);
-
-    if (selectedYear == "2016") {
-        console.log("Año seleccionado es 2016");
-        document.querySelector(".canasta_month_option[value='01']").setAttribute("disabled", true);
-        document.querySelector(".canasta_month_option[value='02']").setAttribute("disabled", true);
-        document.querySelector(".canasta_month_option[value='03']").setAttribute("disabled", true);
-        canasta_month_select.value = "04";
-        selectedMonth = canasta_month_select.value;
-
-        console.log("Mes seleccionado:", selectedMonth);
-    } else {
-        console.log("Año NOO 2016");
-
-        if (canasta_month_select.querySelector(".canasta_month_option[value='01']").hasAttribute("disabled")) {
-            document.querySelector(".canasta_month_option[value='01']").removeAttribute("disabled");
-            document.querySelector(".canasta_month_option[value='02']").removeAttribute("disabled");
-            document.querySelector(".canasta_month_option[value='03']").removeAttribute("disabled");
-
-            //canasta_month_select.insertBefore(option01_month, canasta_month_select.children[1]);
-            //canasta_month_select.insertBefore(option02_month, canasta_month_select.children[2]);
-            //canasta_month_select.insertBefore(option03_month, canasta_month_select.children[3]);
         }
     }
-});
 
-canasta_month_select.addEventListener("change", function () {
-    selectedMonth = canasta_month_select.value;
-    console.log("Mes seleccionado:", selectedMonth);
-});
+    let str_years = array_years.join("");
+
+    canasta_year_select.innerHTML = str_years;
+
+    canasta_year_select.addEventListener("change", function () {
+        selectedYear = canasta_year_select.value;
+        console.log("selectedYear:", selectedYear);
+        ///console.log("Mes seleccionado:", selectedMonth);
+
+        if (selectedYear == "2016") {
+            //console.log("Año seleccionado es 2016");
+            document.querySelector(".canasta_month_option[value='01']").setAttribute("disabled", true);
+            document.querySelector(".canasta_month_option[value='02']").setAttribute("disabled", true);
+            document.querySelector(".canasta_month_option[value='03']").setAttribute("disabled", true);
+            canasta_month_select.value = "04";
+            selectedMonth = canasta_month_select.value;
+
+            console.log("Mes seleccionado:", selectedMonth);
+        } else {
+            if (canasta_month_select.querySelector(".canasta_month_option[value='01']").hasAttribute("disabled")) {
+                document.querySelector(".canasta_month_option[value='01']").removeAttribute("disabled");
+                document.querySelector(".canasta_month_option[value='02']").removeAttribute("disabled");
+                document.querySelector(".canasta_month_option[value='03']").removeAttribute("disabled");
+
+                //canasta_month_select.insertBefore(option01_month, canasta_month_select.children[1]);
+                //canasta_month_select.insertBefore(option02_month, canasta_month_select.children[2]);
+                //canasta_month_select.insertBefore(option03_month, canasta_month_select.children[3]);
+            }
+        }
+    });
+
+    canasta_month_select.addEventListener("change", function () {
+        selectedMonth = canasta_month_select.value;
+        console.log("Mes seleccionado 2:", selectedMonth);
+    });
+
+    let cba;
+    let url_cba_cbt =
+        "https://apis.datos.gob.ar/series/api/series/?ids=150.1_CSTA_BARIA_0_D_26,150.1_CSTA_BATAL_0_D_20&collapse=month&start_date=2016-01-01&limit=1000&format=json";
+
+    async function getCbaCbtData() {
+        try {
+            const response = await fetch(url_cba_cbt);
+            const data_cba_cbt = await response.json();
+            const cba_cbt = data_cba_cbt.data;
+
+            // Data en TOP short cba cbt +++++++++++++++++++++++++++
+            cba = Math.round(cba_cbt[cba_cbt.length - 1][1] * 3.09);
+            const cba_top_short = document.querySelector(".indices_short_cba");
+            cba_top_short.innerHTML = `$${cba}`;
+
+            let cbt = Math.round(cba_cbt[cba_cbt.length - 1][2] * 3.09);
+            const cbt_top_short = document.querySelector(".indices_short_cbt");
+            cbt_top_short.innerHTML = `$${cbt}`;
+            //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+            //console.log("cba_cbt", cba_cbt[2][0]);
+
+            selectedYear = canasta_year_select.value;
+            console.log("selectedYear2:", selectedYear);
+
+            for (let i = 0; i < cba_cbt.length; i++) {
+                if (cba_cbt[i][0] == "2022-05-01") {
+                    // console.log("date[i][0]", cba_cbt[i][0]);
+                    // console.log("date[i][0]", cba_cbt[i][0]);
+                    // console.log("cba_cbt[i][1]", cba_cbt[i][2]);
+                }
+            }
+        } catch (error) {
+            console.log("ERROR", error);
+        }
+    }
+
+    getCbaCbtData();
+}
+
+datesMain();
